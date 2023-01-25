1c3f95cbd2a2558280ab07d98b8cf9e2f58bfa3a
zhangsiqi <74521118+zhsiqi@users.noreply.github.com>
2023-01-26 01:23:02 +0800

    重叠多列参考资料时间源数据

--- 拼接数据源.py
+++ 拼接数据源.py
@@ -12,8 +12,7 @@ import numpy as np
 import os
 import matplotlib.pylab as plt
 import sqlite3 as sqlite
-
-
+import re
 
 os.chdir('/Users/zhangsiqi/Desktop/毕业论文代码mini/专门输出数据表/0125补充htmldate时间')
 
@@ -21,15 +20,48 @@ df = pd.read_csv('citation+html2date.csv', index_col='Unnamed: 0')
 
 #查看每列的缺失值
 print(df.isnull().sum())
+#%% 修正百科的参考资料源日期
+for index, row in df.iterrows():
+    text =  row['reference_text']
+    ti = re.search(r'(?<!引用日期)．(?P<y>20[0-2][0-9])[-年\.](?P<m>[0-1]?[0-9])[-月\.](?P<d>[0-3]?[0-9])[日\.\[\s]?', text)
+    if ti:
+        sourcetime = ti.groupdict()['y']+'-'+ti.groupdict()['m']+'-'+ti.groupdict()['d']
+    else:
+        sourcetime = None
+    df.at[index,'source_time'] = sourcetime
 
 
-#第一次直接复制sourcetime
+#写入csv & sql
+os.chdir('/Users/zhangsiqi/Desktop/毕业论文代码mini/专门输出数据表/0125修正百科source时间提取错误')
+
+df.to_csv("citation+html2date-修正soti.csv", index=True)
+
+conn3= sqlite.connect('citation+html2date-修正soti.sqlite')
+df.to_sql('citation+news', conn3, index=True, if_exists = 'replace')
+conn3.close()
+
+print(df.isnull().sum())
+
+#%% 重叠不同源时间
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pylab as plt
+import sqlite3 as sqlite
+import re
+
+os.chdir('/Users/zhangsiqi/Desktop/毕业论文代码mini/专门输出数据表/0125修正百科source时间提取错误')
+
+df = pd.read_csv('citation+html2date-修正soti.csv', index_col='Unnamed: 0')
+
+#直接复制sourcetime到新列,然后将urltime不为空的值重叠到pubtime一列
+#df['pub_time'] = df['source_time'].where(df['source_time'].isna(), df['url_time'])
+
 df['pub_time'] = df['source_time']
+df['pub_time'] = df['pub_time'].combine_first(df['url_time'])
+df['pub_time'] = df['pub_time'].combine_first(df['timestamp'])
 
-#第二次将urltime不为空的值重叠到pubtime一列
-df['pub_time'] = df['pub_time'].where(df['url_time'].notna(), df['url_time'])
-
-conn3= sqlite.connect('test移动.sqlite')
+conn3= sqlite.connect('时间重叠.sqlite')
 df.to_sql('citation', conn3, index=True, if_exists = 'replace')
 conn3.close()
 
