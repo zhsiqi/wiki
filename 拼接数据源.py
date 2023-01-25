9741abb3c5abd8d83cc71ff2fd5929d142be3bb1
zhangsiqi <74521118+zhsiqi@users.noreply.github.com>
2023-01-25 23:12:01 +0800

    update

--- 拼接数据源.py
+++ 拼接数据源.py
@@ -11,17 +11,40 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pylab as plt
+import sqlite3 as sqlite
 
 
 
-os.chdir('/Users/zhangsiqi/Desktop/毕业论文代码mini/专门输出数据表/0124补充卫健委等时间')
+os.chdir('/Users/zhangsiqi/Desktop/毕业论文代码mini/专门输出数据表/0125补充htmldate时间')
 
-df = pd.read_csv('citation+news-nhc-6.csv',index_col='Unnamed: 0')
+df = pd.read_csv('citation+html2date.csv', index_col='Unnamed: 0')
 
-#%% status_code 修正
+#查看每列的缺失值
+print(df.isnull().sum())
+
+
+#第一次直接复制sourcetime
+df['pub_time'] = df['source_time']
+
+#第二次将urltime不为空的值重叠到pubtime一列
+df['pub_time'] = df['pub_time'].where(df['url_time'].notna(), df['url_time'])
+
+conn3= sqlite.connect('test移动.sqlite')
+df.to_sql('citation', conn3, index=True, if_exists = 'replace')
+conn3.close()
+
+#%% status_code 修正，后面有空再说
 
 
 
+#%% cnOCR
+from cnocr import CnOcr
+
+img_fp = '/Users/zhangsiqi/Desktop/毕业论文代码mini/testrm.png'
+ocr = CnOcr(det_model_name='naive_det')  # 所有参数都使用默认值
+out = ocr.ocr(img_fp)
+
+print(out)
 
 
 #%%
