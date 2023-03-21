3d69c56c5d28a5a68d4d4fc0124e6b81e77301a6
Siqi Zhang <74521118+zhsiqi@users.noreply.github.com>
2023-03-22 03:42:32 +0800

    up

--- 数据爬取-新闻原网址-补.py
+++ 数据爬取-新闻原网址-补.py
@@ -67,7 +67,7 @@ driver.quit()
 df.to_csv('ci+原始链接补15s.csv',index=True)
 
 # conn = sqlite.connect('/Users/zhangsiqi/Desktop/毕业论文代码mini/专门输出数据表/0214补充词条数据/Wiki+1.sqlite')
-df.to_sql('ci+15s', conn, index=True)
+df.to_sql('ci+15s', conn, index=False, if_exists = 'replace')
 conn.close()
 
 over_count = ((df['original_url'] == '15s超时')|(df['original_url'] == 'driver错误')).sum()
@@ -82,7 +82,11 @@ from seleniumwire import webdriver
 import datetime
 import sqlite3 as sqlite
 
-df = pd.read_csv('ci+原始链接补15s.csv',index_col=0)
+conn = sqlite.connect('/Users/zhangsiqi/Documents/毕业论文数据/专门输出数据表/0319/Wiki+1.sqlite')
+c = conn.cursor()
+df = pd.read_sql("""SELECT * FROM 'ci+15s'""", conn)
+
+#df = pd.read_csv('ci+原始链接补15s.csv',index_col=0)
 
 driver = webdriver.Chrome('chromedriver')
 
@@ -98,9 +102,9 @@ for index, row in df.iterrows():
             driver.switch_to.window(driver.window_handles[-1])
             link = driver.current_url
         except TimeoutException:
-            df.at[index, 'original_url'] = '100s超时'
+            df.at[index, 'original_url'] = '200s超时'
             df.at[index, 'status_collect_time'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print(index, '100s超时')
+            print(index, '200s超时')
         except UnexpectedAlertPresentException:
             driver.switch_to.alert.accept()
             df.at[index, 'original_url'] = link
@@ -119,19 +123,43 @@ for index, row in df.iterrows():
     
 driver.quit()
 
-df.to_csv('ci+100s.csv',index=False)
+df.to_csv('ci+200s.csv',index=False)
 
 # conn = sqlite.connect('/Users/zhangsiqi/Desktop/毕业论文代码mini/专门输出数据表/0214补充词条数据/Wiki+1.sqlite')
-df.to_sql('ci+100s', conn, index=False)
+df.to_sql('ci+200s', conn, index=False, if_exists = 'replace')
 conn.close()
 
-over_count = ((df['original_url'] == '100s超时')|(df['original_url'] == 'driver错误')).sum()
+over_count = ((df['original_url'] == '200s超时')|(df['original_url'] == 'driver错误')).sum()
 print('后面需要处理的超时链接个数为', over_count)
 
-df.loc[pd.isna(df['origin_url']) & pd.notna(df['redir_url']),'origin_url']=df['original_url']
 
+#人工补足200s超时
+link1 = 'https://baike.baidu.com/reference/50157160/7fe7rC6hCg-75LPSYfVKa9jJBDcV4OEDKrIFTF1p26BhcYDvoVqJg3oa3VotAjP_dwfeZjjJIkp7W_AtfDiuehmAzUqgX7UviMypxg'
+link2 = 'https://baike.baidu.com/reference/8034979/aef5PvzGRcB5u1ZK8-cypHU9FCZilhSE_xYA8UBpXIGWhum9LzbbByG88Tj281fFsQ5y2VlSQYMe2IM4O4UxQyHX6XlhvaZ0CnMd5v0'
+link3 = 'https://baike.baidu.com/reference/8034979/2b72Fm8ke9A5-tK8og8PFTRZ6CSslr829jAgViC19s-vINrCO6ktJvBjBGuxSyNycsEE1upDgFhbaf84pv-npr95GuLh-LlvtmWnAMZSg7vCNOUefI1nwCuH2arR'
+link4 = 'https://baike.baidu.com/reference/59764769/b423nPS5fStJ9yKodHRj8nBJRN8GG_IP_H_4gaT7VxfFSEu1d427bsgRwcF51IKIDuK07IqChgKp-0uctg1yA7-kv_-Df9VfKJl3'
+link5 = 'https://baike.baidu.com/reference/12061628/8b17ZC4wBAqWOvpYjtfS05TgE-5DYAGDvtO-PMpHxZRHZonxUr_uenZobSYaD4WXatYRv3hSVcsXfLoPML9faAk8r_MhUAg8'
+
+tlink1 = 'https://www.gcs.gov.mo/detail/zh-hant/N20BDLplKR'
+tlink2 = 'https://www.ettoday.net/news/20150319/477032.htm'
+tlink3 = 'https://ent.sina.com.cn/s/h/2022-05-09/doc-imcwiwst6464205.shtml'
+tlink4 = 'https://taiwan.huanqiu.com/article/48wOcNacEgW'
+tlink5 = 'https://olympics.com/en/olympic-games/beijing-2022/mascot'
+
+
+conn = sqlite.connect('/Users/zhangsiqi/Documents/毕业论文数据/专门输出数据表/0319/Wiki+1.sqlite')
+c = conn.cursor()
+df = pd.read_sql("""SELECT * FROM 'ci+200s'""", conn)
+
+df.loc[df['reference_url']==link1,'original_url'] = tlink1
+df.loc[df['reference_url']==link2,'original_url'] = tlink2
+df.loc[df['reference_url']==link3,'original_url'] = tlink3
+df.loc[df['reference_url']==link4,'original_url'] = tlink4
+df.loc[df['reference_url']==link5,'original_url'] = tlink5
+
+df.loc[pd.isna(df['origin_url']) & pd.notna(df['redir_url']),'origin_url']=df['original_url']
 # conn = sqlite.connect('/Users/zhangsiqi/Desktop/毕业论文代码mini/专门输出数据表/0214补充词条数据/Wiki+1.sqlite')
-df.to_sql('ci', conn, index=False, if_exists='replace')
+df.to_sql('ci2023-03-21', conn, index=False, if_exists='replace')
 conn.close()
 
 # #%%300s为限获取链接
